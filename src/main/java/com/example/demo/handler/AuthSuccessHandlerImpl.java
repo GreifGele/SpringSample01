ac793c36ac424a8dbae35345e0ebb53db5486542
GreifGele <greifen.gelegenheit@gmail.com>
2023-06-11 23:29:13 +0900

    モバイル判断を追加。Auth成功時についてもUtilsに移管。

--- src/main/java/com/example/demo/handler/AuthSuccessHandlerImpl.java
+++ src/main/java/com/example/demo/handler/AuthSuccessHandlerImpl.java
@@ -3,7 +3,6 @@ package com.example.demo.handler;
 import java.io.IOException;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import jakarta.servlet.ServletException;
@@ -15,11 +14,7 @@ public class AuthSuccessHandlerImpl implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
             Authentication authentication) throws IOException, ServletException {
-                // mobile の場合は 200 ok
-                //response.setStatus(HttpStatus.OK.value());
-
-                // pc の場合は指定のページ
-                new DefaultRedirectStrategy().sendRedirect(request, response, "/hello");
+                HandlerUtils.onAuthSuccess(request, response);
     }
     
 }
--- src/main/java/com/example/demo/handler/HandlerUtils.java
+++ src/main/java/com/example/demo/handler/HandlerUtils.java
@@ -1,5 +1,7 @@
 package com.example.demo.handler;
 
+import java.io.IOException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.security.web.DefaultRedirectStrategy;
 
@@ -7,12 +9,31 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class HandlerUtils {
-    public static void onAuthFailure(HttpServletRequest request, HttpServletResponse response) {
-                // mobile の場合は 401エラー
-                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+    public static boolean isMobile(HttpServletRequest request) {
+        String ua = request.getHeader("user-agent");
+        return ua.toLowerCase().contains("mobile");
+    }
 
-                // pc の場合は session を破棄して login 画面に遷移
-                //request.getSession().invalidate();
-                //new DefaultRedirectStrategy().sendRedirect(request, response, "/login");
+    public static void onAuthFailure(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if (isMobile(request)) {
+            // mobile の場合は 401エラー
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        }
+        else {
+            // pc の場合は session を破棄して login 画面に遷移
+            request.getSession().invalidate();
+            new DefaultRedirectStrategy().sendRedirect(request, response, "/login");
+        }
+    }
+
+    public static void onAuthSuccess(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if (isMobile(request)) {
+            // mobile の場合は 200 ok
+            response.setStatus(HttpStatus.OK.value());
+        }
+        else {
+            // pc の場合は指定画面に遷移
+            new DefaultRedirectStrategy().sendRedirect(request, response, "/hello");
+        }
     }
 }
